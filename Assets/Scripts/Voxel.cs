79202fbe6c2b399072196e7db84d40beea6c8d31
Hoooon22 <momo990305@gmail.com>
2022-01-31 17:10:32 +0900

    voxel.cs start

--- Assets/Scripts/Voxel.cs
+++ Assets/Scripts/Voxel.cs
@@ -6,9 +6,16 @@ using UnityEngine;
 
 public class Voxel : MonoBehaviour
 {
+    // 1. voxel's speed
+    public float speed = 5;
+
     void Start()
     {
-        
+        // 2. find random
+        Vector3 direction = Random.insideUnitSphere;
+        // 3. set random speed
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        rb.velocity = direction * speed;
     }
 
     void Update()
