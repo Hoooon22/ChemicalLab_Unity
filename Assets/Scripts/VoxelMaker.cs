abb239a67cd82fa7de0a0c16f1b9f15acb503c8f
Hoooon22 <momo990305@gmail.com>
2022-02-10 21:41:32 +0900

    sync

---
+++ Assets/Scripts/VoxelMaker.cs
@@ -0,0 +1,20 @@
+//  사용자가 마우스를 클릭한 지점에 복셀 생성
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoxelMaker : MonoBehaviour
+{
+    // voxel factory
+    public GameObject voxelFactory;
+
+    void Update()
+    {
+        // 1. if click mouse
+        if (Input.GetButton("Fire1"))
+        {
+
+        }
+    }
+}
